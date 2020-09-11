4468ed07e6c533f2f5e3b3a447febf1992f3d00c
samadurm <samadurm@oregonstate.edu>
2020-09-11 10:59:20 -0700

    Scan button correctly toggles isScanning bool

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:stay_away_from_me/app.dart';
 
 class ScanButton extends StatefulWidget {
-  @override
    bool isScanning;
-   bool curState;
 
    ScanButton({this.isScanning});
+  @override
   _ScanButtonState createState() => _ScanButtonState();
 }
 
 class _ScanButtonState extends State<ScanButton> {
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -20,15 +17,15 @@ class _ScanButtonState extends State<ScanButton> {
       child: FloatingActionButton(
         child: Text(decideButtonText(widget.isScanning), textScaleFactor: 1.3,),
         onPressed: () {
-          // setState(){
-          //   widget.curState = toggleScan(widget.curState);
-          // }
-          print('Toggling isScanning: ${widget.isScanning}');
+          setState( () {
+            widget.isScanning = toggleScan(widget.isScanning);
+          });
         },
       ),
     );
   }
 }
+
 String decideButtonText(bool isScanning) => isScanning ? 'Stop' : 'Scan';
 
 bool toggleScan(bool isScanning) => isScanning ? false : true; 
