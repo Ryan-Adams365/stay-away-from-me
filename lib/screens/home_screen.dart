74dfee3254bcbf13507ff19fb5414caea7f619d1
samadurm <samadurm@oregonstate.edu>
2020-09-10 17:44:28 -0700

    Added horizontal padding based on the screen size

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:stay_away_from_me/functions/functions.dart';
 
 class HomeScreen extends StatelessWidget {
   
   static final int count = 0;
-  static final double horizontalPadding = 10;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: horizontalPadding, right: horizontalPadding),
+      padding: EdgeInsets.only(left: getPaddingAmount(context, 0.05), right: getPaddingAmount(context, 0.05)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('${count} Nearby Devices', textScaleFactor: 1.5),
-          SizedBox(height: 10,),
-          Placeholder(),
+          Padding(
+            padding: EdgeInsets.all(10)
+          ),
+          Flexible(
+            child: FractionallySizedBox(
+              widthFactor: 1.0,
+              heightFactor: 0.8,
+              child: Placeholder(),
+              ),
+            )
+
         ],
       ),
     );
   }
 }
+
