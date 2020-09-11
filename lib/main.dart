5884669acbf42884d61d550e0f2f17b75a40200c
samadurm <samadurm@oregonstate.edu>
2020-09-10 17:05:08 -0700

    Added device orientation code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stay_away_from_me/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.portraitUp
+  ]);
   runApp(App());
 }
