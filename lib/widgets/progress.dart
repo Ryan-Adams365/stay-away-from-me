aa5cb02360ba984818be3ee4f0ec9b73159433f5
samadurm <samadurm@oregonstate.edu>
2020-09-11 18:09:10 -0700

    Added feature where the phone will vibrate if within close proximity

--- lib/widgets/progress.dart
+++ lib/widgets/progress.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:stay_away_from_me/functions/functions.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+import 'package:vibrate/vibrate.dart';
+import 'package:stay_away_from_me/functions/functions.dart';
 
 
 class Progress extends StatelessWidget {
@@ -12,7 +13,7 @@ class Progress extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double signalStrength = getSnapshotData(parentSnapshot);
-
+    vibrateIfClose(signalStrength);
     return LiquidLinearProgressIndicator(
       value: signalStrength, 
       valueColor: AlwaysStoppedAnimation(colorMap(signalStrength)),
@@ -42,4 +43,22 @@ double getSnapshotData(AsyncSnapshot<double> snapshot){
     return snapshot.data;
   }
   return 0.0;
+}
+
+void vibrateIfClose(double signalStrength) async {
+  if(signalStrength >= 0.7){
+    bool canVibrate = await Vibrate.canVibrate;
+    if(canVibrate){
+      if(signalStrength >= 0.8){
+        final Iterable<Duration> pauses = [
+            const Duration(milliseconds: 500),
+            const Duration(milliseconds: 1000),
+            const Duration(milliseconds: 500),
+        ];
+        Vibrate.vibrateWithPauses(pauses);
+      } else {
+        Vibrate.vibrate();
+      }
+    }
+  }
 }
