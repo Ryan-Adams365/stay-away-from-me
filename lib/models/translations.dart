1c7ceda331c74f17740fb63a7f08d65ad04cf72c
samadurm <samadurm@oregonstate.edu>
2020-09-11 20:19:24 -0700

    added language support for english, spanish, russian, chinese, and hindi

--- lib/models/translations.dart
+++ lib/models/translations.dart
@@ -14,6 +14,42 @@ class Translations{
       'distance': 'Distance',
       'meters': 'meters' 
     },
+
+    'es': {
+      'appTitle' : 'Mantente Alejado De Mí',
+      'stop': 'Detener',
+      'scan': 'Escanear',
+      'nearDevs': 'Dispositivos Cercanos',
+      'distance': 'Distancia',
+      'meters': 'metros' 
+    },
+
+    'cn': {
+      'appTitle' : '离我远点',
+      'stop': '停止',
+      'scan': '扫瞄',
+      'nearDevs': '附近的设备',
+      'distance': '距离',
+      'meters': '米' 
+    },
+
+    'ru': {
+      'appTitle' : 'Держись Подальше От Меня',
+      'stop': 'Стоп',
+      'scan': 'Поиск',
+      'nearDevs': 'Устройства Поблизости',
+      'distance': 'Расстояние',
+      'meters': 'метры' 
+    },
+
+    'in': {
+      'appTitle' : 'मुझसे दूर रहो',
+      'stop': 'रुकें',
+      'scan': 'स्कैन',
+      'nearDevs': 'निकट के उपकरण',
+      'distance': 'दूरी',
+      'meters': 'मीटर की दूरी पर' 
+    },
   };
 
   String getTranslation(String label) => labels[locale.languageCode][label];
