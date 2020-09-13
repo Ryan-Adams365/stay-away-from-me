d195df23feb0998f835f24f30531b6125c492ed0
samadurm <samadurm@oregonstate.edu>
2020-09-12 18:48:56 -0700

    Added functionality for imperial or metric, will need to switch to prompt widget:

--- lib/widgets/progress.dart
+++ lib/widgets/progress.dart
@@ -7,7 +7,7 @@ import 'package:stay_away_from_me/models/translations.dart';
 
 
 class Progress extends StatelessWidget {
-  
+  bool usingMetric = false;
   final List<ScanResult> deviceList;
 
   Progress({this.deviceList});
@@ -29,10 +29,7 @@ class Progress extends StatelessWidget {
       borderWidth: 1.0,
       borderRadius: 12.0,
       direction: Axis.vertical, 
-      center: deviceList.isNotEmpty ? Text(
-        "${translations.getTranslation('distance')} ${minDistance.toStringAsFixed(2)} ${translations.getTranslation('meters')}",
-        textScaleFactor: 1.2
-      ) : Text(translations.getTranslation('scanning'), textScaleFactor: 1.2,),
+      center: deviceList.isNotEmpty ? distanceText(translations, minDistance, usingMetric) : Text(translations.getTranslation('scanning'), textScaleFactor: 1.2,),
     );
   }
 }
@@ -77,4 +74,17 @@ void vibrateIfClose(double signalStrength) async {
       }
     }
   }
+}
+
+Text distanceText(Translations translations, double distance, bool usingMetric){
+  if(usingMetric){
+    return Text(
+      "${translations.getTranslation('distance')} ${distance.toStringAsFixed(2)} ${translations.getTranslation('meters')}",
+      textScaleFactor: 1.2
+    );
+  }
+  return Text(
+    "Distance ${metersToFt(distance).toStringAsFixed(2)} ft",
+    textScaleFactor: 1.2
+  );
 }
