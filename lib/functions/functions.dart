50281a9a990c42423b044c5fd7842dce3063ee9d
samadurm <samadurm@oregonstate.edu>
2020-09-12 17:43:48 -0700

    Updated the bounds for distance and created functions to normalize the safe and unsafe distance to a value of 0 and 1

--- lib/functions/functions.dart
+++ lib/functions/functions.dart
@@ -14,14 +14,46 @@ double getPaddingAmount(BuildContext context, double factor, bool usingWidth) {
     return padding;
 } 
 
-// function stub
-int getNumNearbyDevices(){
-  return 0;
-}
-
+/*
+  Function: convertToDistance
+  Description: Converts signal stregnth to distance in meters
+*/
 double convertToDistance(int txPowerLevel){
   double measuredPower = -69.0;
   double N = 2.0;
 
   return pow(10, ((measuredPower-txPowerLevel)/(10*N)));
 }
+
+/*
+  Function: convertDistToStrength
+  Description: returns a value between 0 and 1 for a 
+    distance between min and max dist, where a 1 is 
+    given if less than or equal to min dist, or 0 if 
+    greater than or equal to max dist
+*/
+double convertDistToStrength(double distance, double minDist, double maxDist){
+  if(distance <= minDist){
+    return 1;
+  } else if(distance > maxDist){
+    return 0;
+  } else{
+    return scaleValue(distance, minDist, maxDist);
+  }
+}
+
+/*
+  Function: scaleValue
+  Description: scales a value between a lower and upperbound, where 
+    the closer to the lowerbound the value is, the closer to 1 the 
+    returned value is, and the closer to upperbound the value, the 
+    closer to 1 the returned value is
+*/
+double scaleValue(double val, double lowerBound, double upperBound) {
+  double scaled = (val - upperBound) / (lowerBound - upperBound);
+  
+  if(scaled == -0.0){ // this is to avoid a -0 
+    scaled = 0; 
+  }
+  return scaled;
+} 
